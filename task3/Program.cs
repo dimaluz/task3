b60a02f9e5ee7046592eb12f3140d4a0f3a3fb3a
Dmitry Liuzin <dimaluz@rambler.ru>
2019-08-22 17:46:29 +0400

    task3

---
+++ task3/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Threading.Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("en-US");
+            List<Double> [] Cashes = new List<Double>[5];
+
+            String path = args[0];
+            DirectoryInfo di = new DirectoryInfo(path);
+            FileInfo[] files = di.GetFiles();
+            Int32 index = 0;
+
+            foreach (FileInfo file in files) {
+                String filepath = path +@"\"+ file.Name;
+                FileStream fs = new FileStream(filepath, FileMode.Open, FileAccess.Read);
+                StreamReader read = new StreamReader(fs);
+                Cashes[index] = new List<Double>();
+                Boolean isNum; Double value;
+                while (!read.EndOfStream) {
+                    String str = read.ReadLine();
+                    isNum=Double.TryParse(str, out value);
+                    if(isNum)
+                        Cashes[index].Add(value);
+                }
+                index++;
+                read.Close();
+                fs.Close();
+            }
+
+            Double[] results = new Double[16];
+            Double[] intervals = new Double[16];
+            Double sum = 0.0;
+            for (Int32 i = 0; i < 16; i++) {
+                for (Int32 j = 0; j < 5; j++) {
+                    sum += Cashes[j][i];
+                }
+                results[i] = sum; intervals[i] = i + 1;
+                sum = 0.0;
+            }
+            Double val =results[0]; Int32 pos=0;
+            for(Int32 i=0; i < 16; i++)
+                if(val < results[i]){
+                    val = results[i]; pos = i;
+                }
+            Console.WriteLine(pos);
+            Console.Read();
+        }
+    }
+}
---
+++ task3/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("task3")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("task3")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("c0b241c0-25ee-44c2-83dc-c1a86aa18071")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//      Номер построения
+//      Редакция
+//
+// Можно задать все значения или принять номер построения и номер редакции по умолчанию, 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
